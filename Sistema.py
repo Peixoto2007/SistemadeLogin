75ea80069341509c2668b6c0b5174c7fdc25c7dc
Peixoto2007 <dyegolucas2020@gmail.com>
2026-06-02 22:08:16 -0300

    Feito espacos do CREATE e READ

--- Sistema.py
+++ Sistema.py
@@ -1,20 +1,47 @@
 import mysql.connector
 
 conexao = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    password='123456',
-    database='SistemadeLogin'  
+         host='localhost',
+         user='root',
+         password='123456',
+         database='SistemadeLogin'
 )
-
 cursor = conexao.cursor()
+# 
+# CREATE
+# 
+# update ='UPDATE Usuario set SenhaUsuario = "Dyegoprocuraestagioagosto" WHERE id = 1 '
+# 
+# 
+# 
+# insert = 'INSERT into Usuario values ( 2 , "Maria" , "marialuciana1982@" , "maria192luciana@hotmail.com" )'
+# 
+# cursor.execute(update),
+# cursor.execute(insert)
+# 
+# READ
 
-comando = 'select * from Usuario'
-cursor.execute(comando)
-# conexao.commit()
-resultado = cursor.fetchall()
+select_usuario = "SELECT * from Usuario "
+cursor.execute(select_usuario)
+resultado = cursor.fetchall() 
 
-print("Dados da tabela Usuario:")
-for usuario in resultado:
-    print(usuario)
 
+for i in resultado:
+    print (i)
+
+
+
+conexao.commit()
+
+# drop =0 ,  
+# 
+# 
+# def verificar_login(email, senha):
+#    
+    # 
+    # 
+    # )
+    # 
+    # cursor = conexao.cursor()
+    # select = 'Select * from Usuario Where Email * '
+    # 
