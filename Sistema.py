122538ca1d86ee1b10ac247c7dca3f987953d9cc
Peixoto2007 <dyegolucas2020@gmail.com>
2026-06-04 17:04:29 -0300

    Adicionando a validação de Usuario , suas DEFS e  organizando o Ambiente do Sistema

--- Sistema.py
+++ Sistema.py
@@ -1,5 +1,8 @@
 import mysql.connector
 
+
+    
+
 conexao = mysql.connector.connect(
          host='localhost',
          user='root',
@@ -7,41 +10,60 @@ conexao = mysql.connector.connect(
          database='SistemadeLogin'
 )
 cursor = conexao.cursor()
-# 
+
 # CREATE
-# 
+
 # update ='UPDATE Usuario set SenhaUsuario = "Dyegoprocuraestagioagosto" WHERE id = 1 '
-# 
-# 
-# 
+
+
+
 # insert = 'INSERT into Usuario values ( 2 , "Maria" , "marialuciana1982@" , "maria192luciana@hotmail.com" )'
-# 
+
 # cursor.execute(update),
 # cursor.execute(insert)
-# 
+
+
 # READ
 
-select_usuario = "SELECT * from Usuario "
-cursor.execute(select_usuario)
-resultado = cursor.fetchall() 
+select_Banco = "SELECT NomeUsuario from Usuario "
+cursor.execute(select_Banco)
+verbanco = cursor.fetchall()
+
+Nomes = []
+for row in verbanco:
+    print(row)
+    Nomes.append (row[0])
+
+print (Nomes)
+# print(verbanco)
+
+# select_usuario = "SELECT NomeUsuario from Usuario"
+# ver = cursor.execute(select_usuario)
+# for NamesUsu in Seenames:
+#     print (NamesUsu)
 
 
-for i in resultado:
-    print (i)
+
+# conexao.commit()
 
 
 
-conexao.commit()
+#Backeeend manito
+#funcoes
+def cadastro():
+    while True:
+        print("Vamos te cadastrar em Nosso Banco de Dados")
+        User= input("Digita o Nome do seu usuario :  ")
+        Email = input ("Your best Email :  ")
+        if User in Nomes: 
+            print( 'Invalid')
+            continue
+        else : break
 
-# drop =0 ,  
-# 
-# 
-# def verificar_login(email, senha):
-#    
-    # 
-    # 
-    # )
-    # 
-    # cursor = conexao.cursor()
-    # select = 'Select * from Usuario Where Email * '
-    # 
+#funcoes
+print("Bem Vindo ao sistema de login ")
+pergunta1 = input("e seu primeiro login ?")
+if pergunta1.lower() in {"sim","s","si","ss"}:
+    cadastro()
+
+    
